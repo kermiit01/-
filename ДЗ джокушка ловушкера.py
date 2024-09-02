a3eee609e531cc795a205f4db372896b23638878
kermiit01 <kermitegor@gmail.com>
2024-09-02 20:11:23 +0800

    Add files via upload

---
+++ ДЗ джокушка ловушкера.py
@@ -0,0 +1,11 @@
+import random
+n=random.randint(3,20)
+print(n)
+result=[]
+for i in range(n):
+    for a in range(n):
+        if i+a==n:
+            result.append(i)
+            result.append(a)
+result_final=''.join(map(str,result))
+print(result_final)
