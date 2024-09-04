7fecbf3a5437ba137a3c92c8c5d5f6f885d18380
kermiit01 <kermitegor@gmail.com>
2024-09-04 16:40:26 +0800

    Update ДЗ джокушка ловушкера.py
    Задание "Слишком древний шифр"

--- ДЗ джокушка ловушкера.py
+++ ДЗ джокушка ловушкера.py
@@ -4,8 +4,9 @@ print(n)
 result=[]
 for i in range(n):
     for a in range(n):
-        if i+a==n:
-            result.append(i)
-            result.append(a)
+        if i<a:
+            if i+a==n:
+                result.append(i)
+                result.append(a)
 result_final=''.join(map(str,result))
 print(result_final)
