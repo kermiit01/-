1b25e670ca7b69849e7b0dbe3ca1f01dc9f093ab
kermiit01 <kermitegor@gmail.com>
2025-01-15 18:18:36 +0800

    Деление на 0

---
+++ fake_math.py
@@ -0,0 +1,6 @@
+def divide(first,second):
+    if second==0:
+        print('Ошибка')
+        return
+    result=first/second
+    print(round(result,3))
---
+++ true_math.py
@@ -0,0 +1,7 @@
+from math import inf
+def divide(first,second):
+    if second==0:
+        print(inf)
+    result=first/second
+    print(round(result,3))
+    return inf
