789e33b42d65e929ae268b66983c58bd6b282081
kermiit01 <kermitegor@gmail.com>
2025-01-27 12:48:39 +0800

    module 6_1

---
+++ Зачем нужно наследование.py
@@ -0,0 +1,44 @@
+class ecosystem:
+    name = ''
+
+    def __init__(self, name):
+        self.name = name
+class Animal(ecosystem):
+    alive = True
+    fed = False
+    def eat(self,food):
+        if food.edible ==  True:
+            Animal.fed = True
+            print(f'{self.name} съел {food.name}')
+        else:
+            Animal.alive = False
+            print(f'{self.name} не стал есть {food.name} и погиб(((')
+
+
+class Plant(ecosystem):
+    edible = False
+class Mammal(Animal):
+    pass
+class Predator(Animal):
+    def __init__(self,name):
+        self.name = name
+    pass
+class Flower(Plant):
+    pass
+class Fruit(Plant):
+    edible = True
+
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
+
+print(a1.name)
+print(p1.name)
+
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
